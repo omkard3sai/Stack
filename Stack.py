114443376fe82708cfb96170a9c8047b965779bd
omkard3sai <omkar.desai@utdallas.edu>
2017-06-18 18:20:43 -0500

    Replaced list() with node object

--- Stack.py
+++ Stack.py
@@ -1,39 +1,45 @@
+from Node import Node
+
+
 class Stack:
 
-    def __init__(self, size):
-        self.stack = list()
-        self.size = size
+    def __init__(self):
+        self.top = None
 
-    def push(self, item=20):
-        if not self.isfullstatus():
-            self.stack.append(item)
-            return True
+    def push(self, item):
+        insert = Node(item)
+        if self.empty_status():
+            insert.below = None
+            self.top = insert
         else:
-            return False
+            insert.below = self.top
+            self.top = insert
+        return True
 
     def pop(self):
-        if not self.isemptystatus():
-            return self.stack.pop()
+        if not self.empty_status():
+            pop_data = self.top.data
+            self.top = self.top.below
+            return pop_data
         else:
             return False
 
     def peek(self):
-        if not self.isemptystatus():
-            return self.stack[-1]
+        if not self.empty_status():
+            return self.top.data
         else:
             return False
 
-    def isfullstatus(self):
-        if len(self.stack) == self.size:
-            return True
-        else:
-            return False
-
-    def isemptystatus(self):
-        if not self.stack:
+    def empty_status(self):
+        if self.top is None:
             return True
         else:
             return False
 
     def print(self):
-        print('->', ','.join(str(a) for a in reversed(self.stack)))
+        stack = self.top
+        print("Top/Push/Pop", end=" -> ")
+        while stack:
+            print(stack.data, end=" -> ")
+            stack = stack.below
+        print("Bottom")
